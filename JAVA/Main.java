c0071796fcf6c9f23ef989ce09c841d03248acbb
vasudha <vasudhabhatkp@gmail.com>
2025-09-25 18:27:25 +0530

    add

---
+++ JAVA/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+
+        public static void main(String[] args){
+
+            System.out.println("Hello World!!"); // Println for new line or can add "\n"
+            System.out.print("Hi everyone\n"); // This is a comment
+            System.out.print("How are you??"); //sout + tab
+        /* Variable = a reusable container for a value
+                      a variable behaves as if it was the value it contains
+                      Primitive --> simple value stored directly in memory(stack) int,double,char,boolean
+                      Reference --> memory address(stack) that points to heap   string,array,object
+         Steps to creating a variable
+         1.declaration
+         2.assignment
+
+         */
+            int age;
+
+        }
+
+// This is a comment
+/* This
+    is
+    a
+    multi-line
+    comment
+ */
+}
